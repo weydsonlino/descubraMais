6c2abb15160c0a38d3de264d93c16deac9623a61
crocodileBigger <infobighenrique@gmail.com>
2024-12-27 01:20:05 -0300

    atualização do cadastro

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { RouterLink } from 'vue-router'
+</script>
+<template>
+  <header id="header">
+    <RouterLink to="/"><img src="../assets/DescubraMaisLogo.png" alt="" srcset="" /></RouterLink>
+    <nav id="header-Navigation">
+      <RouterLink to="/adicionarPontoTuristico" class="header-link">Pontos Turistico</RouterLink>
+      <RouterLink to="/about" class="header-link">Sobre nós</RouterLink>
+      <RouterLink to="/login" class="button header-link">Entrar</RouterLink>
+      <RouterLink to="/cadastroUsuario" class="button register-button header-link">Cadastrar</RouterLink>
+    </nav>
+  </header>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1>Initial Page</h1>
+  </main>
+</template>
