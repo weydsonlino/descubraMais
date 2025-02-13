7b23983836a82e0bbb3b93205ecb1aac1440790e
Letdiasc <marialetciadias@gmail.com>
2025-02-12 22:45:05 -0300

    componetizei a pagina

--- descubraMais/src/views/HomeView.vue
+++ descubraMais/src/views/HomeView.vue
@@ -2,6 +2,55 @@
 
 <template>
   <main>
-    <div><img src="../assets/ilha-das-cobras_df0840a5_240502112852_240701112339_1280x943.png" alt=""></div>
+    <div id="imagem_inicial">
+      <input id="caixaPesquisa" type="text">
+      <label for="caixaPesquisa">o que voce deseja</label>
+
+
+    </div>
+    <div>
+      <h1>conheça alguns pontos famosos</h1>
+    </div>
+
+    <div>
+      <div>
+        <img src="../assets/imagemi_igreja_sao_goncalo.png" alt="">
+        <button>visitar</button>
+      </div>
+      <div>
+        <img src="../assets/imagem_mosteiro_de_são_bento.png" alt="">
+        <button>visitar</button>
+      </div>
+      <div>
+        <img src="../assets/imagem_recife_antigo.png" alt="">
+        <button>visitar</button>
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+
+    <div id="descubraMais">
+      <div>
+        <h1>Descubramais</h1>
+        <p>O DescubraMais é o seu guia perfeito para explorar os lugares mais incríveis! Aqui você encontra informações
+          detalhadas sobre pontos turísticos, desde paisagens naturais e trilhas até monumentos históricos e atrações
+          culturais.
+          Mas não para por aí: nosso site é feito para tornar sua experiência ainda mais interativa! Você pode explorar
+          trilhas, escolher seus roteiros favoritos, compartilhar suas aventuras com os amigos e até postar fotos dos
+          momentos mais memoráveis. Além disso, temos um sistema de avaliação para que você ajude outros viajantes a
+          encontrar as melhores experiências.</p>
+        <P>Planeje, explore, compartilhe e viva momentos inesquecíveis com a gente!</P>
+      </div>
+      <img src="../assets/imagem_da_moça da pag inicial.png" alt="">
+    </div>
   </main>
 </template>
+
+<style scoped>
+#imagem_inicial {
+  background-image: url('../assets/imagem_tela_inicial.png');
+  width: 100%;
+  height: 500px;
+}
+</style>
