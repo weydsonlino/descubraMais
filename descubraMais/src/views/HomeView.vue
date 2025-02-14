eb0d8f6b887cca7c587bbe7a6edae8a15786baeb
JoanaG0mes <joana.gomes2377@gmail.com>
2025-02-13 21:09:03 -0300

    pagina inicial com html e css.

--- descubraMais/src/views/HomeView.vue
+++ descubraMais/src/views/HomeView.vue
@@ -1,7 +1,110 @@
 <script setup lang="ts"></script>
-
 <template>
   <main>
-    <div><img src="../assets/ilha-das-cobras_df0840a5_240502112852_240701112339_1280x943.png" alt=""></div>
-  </main>
+    <div class="ilha-das-cobras">
+      <input type="text" id="search-bar" placeholder="Para onde você quer ir hoje ?">
+      <a href="#">
+        <img class="search-icon" src="http://www.endlessicons.com/wp-content/uploads/2012/12/search-icon.png" alt="Ícone de busca">
+      </a>
+    </div>
+
+    <div class="descubra-e-compartilhe">
+      Descubra e Compartilhe os Melhores Pontos Turísticos!
+    </div>
+    <div class="conectamos-guias-e">
+      Conectamos guias e turistas para explorar o mundo juntos
+    </div>
+
+    <div class="conhea-alguns-pontos-famosos-parent">
+      <div class="conhea-alguns-pontos">Conheça Alguns Pontos Famosos</div>
+      <div class="container-imagens">
+        <div class="imagem1">
+          <div class="card1">
+            <div class="tituloMap">Igreja de São Gonçalo do Amarante</div>
+            <div class="h3">Praça 7 de Setembro</div>
+            <div class="h4">Igarassu-PE</div>
+               <div class="botao">
+      <div class="visitar">Visitar</div>
+      </div>
+          </div>
+        </div>
+        <div class="imagem2">
+          <div class="card2">
+            <div class="tituloMap">Instituto Ricardo Brennand</div>
+            <div class="h3">Alameda Antônio Brennand, s/n</div>
+            <div class="h4">Recife-PE</div>
+              <div class="botao">
+      <div class="visitar">Visitar</div>
+      </div>
+          </div>
+        </div>
+        <div class="imagem3">
+          <div class="card3">
+            <div class="tituloMap">Mosteiro de São Bento</div>
+            <div class="h3">Praça 15 de Novembro</div>
+            <div class="h4">Olinda-PE</div>
+           <div class="botao">
+      <div class="visitar">Visitar</div>
+      </div>
+          </div>
+        </div>
+        <div class="imagem4">
+          <div class="card4">
+            <div class="tituloMap">Praia do Forte Orange</div>
+            <div class="h3">Forte Orange</div>
+            <div class="h4">Ilha de Itamaracá-PE</div>
+              <div class="botao">
+      <div class="visitar">Visitar</div>
+      </div>
+          </div>
+        </div>
+        <div class="imagem5">
+          <div class="card5">
+            <div class="tituloMap">Alto da Sé</div>
+            <div class="h3">Rua Bispo Coutinho, s/n, Carmo</div>
+            <div class="h4">Olinda-PE</div>
+               <div class="botao">
+      <div class="visitar">Visitar</div>
+      </div>
+          </div>
+        </div>
+        <div class="imagem6">
+          <div class="card6">
+            <div class="tituloMap">Recife Antigo</div>
+            <div class="h3">Marco Zero</div>
+            <div class="h4">Recife-PE</div>
+        <div class="botao">
+      <div class="visitar">Visitar</div>
+      </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="rectangle-div">
+      <div class="descubramais">DescubraMais</div>
+       <div class="MeninaFeliz"></div>
+      <div class="o-descubramais-container">
+        <div class="o-descubramais">
+          O DescubraMais é o seu guia perfeito para explorar os lugares mais incríveis! Aqui você encontra informações detalhadas sobre pontos turísticos, desde paisagens naturais e trilhas até monumentos históricos e atrações culturais.
+        </div>
+<div class="texto">
+      Planeje, explore, compartilhe <span class="textoCor">e viva momentos inesquecíveis com a gente!</span>.
+    </div>
+    </div>
+    </div>
+<div class="por-que-escolher-nossa-platafo-parent">Por que escolher nossa plataforma?</div>
+<div class="container">
+    <div class="planeta">Explore destinos únicos adicionados por guias locais</div>
+    <div class="Map">Adicione seus pontos turistico</div>
+    <div class="estrela">Interaja, compartilhe experiências e receba dicas de pessoas que amam explorar o mundo, assim como você.</div>
+    <div class="amizade">Avaliações e comentários para encontrar os melhores lugares</div>
+        <div class="planetaIcon"></div>
+    <div class="MapIcon"></div>
+    <div class="estrelaIcon"></div>
+    <div class="amizadeIcon"></div>
+</div>
+
+</main>
 </template>
+
